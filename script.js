45f9b1b0e82ea142bd46f215eb5b5233857c5d8e
paradellajean <102156377+j3anp07@users.noreply.github.com>
2026-05-24 11:16:02 -0300

    Implementando Menu Hambuguer  e Validação de Formulário
    Menu Hambuguer adicionado apenas para mobile de forma responsiva. Validação via Fetch API utilizando o Webhook.

---
+++ script.js
@@ -0,0 +1,69 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+
+navLinks.classList.toggle('ativo');
+
+});
+
+
+const btnEnviar = document.getElementById('btn-enviar');
+const contatoForm = document.querySelector('.contato form');
+const formStatus = document.getElementById('form-status');
+
+if (btnEnviar && contatoForm) {
+  
+  btnEnviar.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    
+    const nomeInput = contatoForm.querySelector('input[type="text"]');
+    const emailInput = contatoForm.querySelector('input[type="email"]');
+    const mensagemInput = contatoForm.querySelector('textarea');
+
+    
+    if (!nomeInput.value || !emailInput.value || !mensagemInput.value) {
+      formStatus.style.color = '#ff4757';
+      formStatus.textContent = 'Por favor, preencha todos os campos.';
+      return;
+    }
+
+    formStatus.style.color = '#333';
+    formStatus.textContent = 'Enviando mensagem...';
+
+    const dadosFormulario = {
+      nome: nomeInput.value,
+      email: emailInput.value,
+      mensagem: mensagemInput.value
+    };
+
+    
+    const webhookUrl = 'https://webhook.site/3e165fc7-006a-498f-b530-46fdcd297e0e';
+
+    fetch(webhookUrl, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dadosFormulario)
+    })
+    .then(response => {
+      if (response.ok) {
+        formStatus.style.color = '#2ed573';
+        formStatus.textContent = 'Mensagem enviada com sucesso!';
+        contatoForm.reset();
+      } else {
+        formStatus.style.color = '#ff4757';
+        formStatus.textContent = 'Erro no servidor do Webhook.';
+      }
+    })
+    .catch(error => {
+      formStatus.style.color = '#ff4757';
+      formStatus.textContent = 'Erro de conexão com o servidor.';
+      console.error('Erro detalhado:', error);
+    });
+  });
+}
